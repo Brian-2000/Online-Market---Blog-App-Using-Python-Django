063b41234c0b71091e51c0d4bfccd4fac39e1582
brian-2000 <mucherebrian@gmail.com>
2022-11-06 14:45:21 +0300

    My first commit

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Brian\Documents\Python-projects\django-projects\code\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
